77aec291de486cf60fbc151c76e51ec5813e937a
tfermoso <tomas.fermoso@gmail.com>
2025-04-02 19:21:37 +0200

    Film Model and Repository

---
+++ src/main/java/com/example/demo/model/Film.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer film_id;
+    private String title;
+    private String description;
+
+    public Film() {
+    }
+
+    public Integer getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(Integer film_id) {
+        this.film_id = film_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
